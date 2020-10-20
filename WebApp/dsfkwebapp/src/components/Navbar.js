90135ce735c40a748e2e5f06fec411ecbaaa5099
Vihaan Manchanda <vihaanmanchanda@berkeley.edu>
2020-10-19 19:30:41 -0700

    components folder made, navbar added

---
+++ WebApp/dsfkwebapp/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React,{useState} from 'react'
+
+function Navbar() {
+    return (
+        <>
+        <nav className = "navbar">
+            <div className = "navbar-container">
+            <Link to = "/" ckassName= "navbar-logo"> 
+            TRVL
+            </Link>
+            </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
