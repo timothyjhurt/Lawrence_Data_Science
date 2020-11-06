6912bc51a12ca6bf490191c0158c57c7e0e0c64d
Vihaan Manchanda <vihaanmanchanda@berkeley.edu>
2020-11-05 19:04:21 -0800

    navbar done

--- WebApp/dsfkwebapp/src/components/Button.js
+++ WebApp/dsfkwebapp/src/components/Button.js
@@ -5,9 +5,14 @@ import { Link } from 'react-router-dom';
 const STYLES = ['btn--primary', 'btn--outline'];
 const SIZES = ['button--medium', 'button--large'];
 
-export const Button = ({children, type, onClick, buttonStyle, buttonSize}) => {
-  const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
-  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+export const Button = 
+({children, type, onClick, buttonStyle, buttonSize}) => {
+  const checkButtonStyle = STYLES.includes(buttonStyle) 
+  ? buttonStyle 
+  : STYLES[0];
+  const checkButtonSize = SIZES.includes(buttonSize) 
+  ? buttonSize
+   : SIZES[0];
 
   return (
     <Link to='/sign-up' className = 'btn-mobile'>
