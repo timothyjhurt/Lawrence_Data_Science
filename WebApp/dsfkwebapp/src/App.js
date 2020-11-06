348d0bc50796589e2c53dd0c64bd3f6fac4e4abb
Vihaan Manchanda <vihaanmanchanda@berkeley.edu>
2020-11-05 23:52:49 -0800

    semi finished home page

--- WebApp/dsfkwebapp/src/App.js
+++ WebApp/dsfkwebapp/src/App.js
@@ -3,13 +3,14 @@ import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route } from
 'react-router-dom';
 import './App.css';
+import Home from './components/pages/Home'
 function App() {
   return (
   <>
   <Router>
     <Navbar />
     <Switch>
-      <Route path = '/' exact />
+      <Route path = '/' exact component = {Home}/>
       </Switch>
     </Router>
     </>
