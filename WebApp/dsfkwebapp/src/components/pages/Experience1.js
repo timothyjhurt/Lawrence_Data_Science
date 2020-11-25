e47f99f771ae9722d1daec6b458d1c796d372447
Vihaan Manchanda <vihaanmanchanda@berkeley.edu>
2020-11-25 03:29:02 -0800

    placeholder

--- WebApp/dsfkwebapp/src/components/pages/Experience1.js
+++ WebApp/dsfkwebapp/src/components/pages/Experience1.js
@@ -7,7 +7,8 @@ export default function Products() {
   const props = useSpring({
     from: { left: '0%', top: '0%', width: '0%', height: '0%', background: 'lightgreen' },
     to: async next => {
-      while (1) {
+      var loopender = 0;
+      while (!loopender) {
         await next({ left: '0%', top: '0%', width: '100%', height: '100%', background: 'lightblue' })
         await next({ height: '50%', background: 'lightgreen' })
         await next({ width: '50%', left: '50%', background: 'lightgoldenrodyellow' })
@@ -17,7 +18,9 @@ export default function Products() {
         await next({ width: '50%', background: 'lightseagreen' })
         await next({ top: '0%', height: '100%', background: 'lightskyblue' })
         await next({ width: '100%', background: 'lightslategrey' })
+        loopender+=1;
       }
+      
     },
   })
     
